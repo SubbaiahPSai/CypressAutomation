03438da4ab1f99570861fb3b3bf948b71a6c4245
SubbaiahPsai <psaichandhu251@gmail.com>
2024-06-19 11:40:19 +0530

    new update

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
+    projectId: "twcy9h",
     "baseurl":"https://opensource-demo.orangehrmlive.com",
     experimentalStudio:true,
     setupNodeEvents(on, config) {
